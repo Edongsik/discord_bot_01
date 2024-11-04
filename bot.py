82a0379cf6772fdf419c636611813b273f95dd4a
edsgames <iusb0605@gmail.com>
2024-11-04 18:34:27 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ import os
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
-@bot.command(name='hello')
+@bot.command(name='인사')
 async def hello(ctx):
     await ctx.send('Hello!')
 
