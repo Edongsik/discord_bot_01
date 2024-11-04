8ce3265111582be965fc69354b0e16809212e6c3
edsgames <iusb0605@gmail.com>
2024-11-04 18:01:37 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run(TOKEN)
